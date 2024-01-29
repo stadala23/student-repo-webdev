911ee483fffc919ff80901bacefec6d850ca9370
stadala <stadala@pdx.edu>
2024-01-28 23:39:05 -0800

    HW1 Submissions

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Srihari Tanmay Karthik Tadala (he/him). I am pursuing my masters degree in computer science with an interest in web development. I worked as a software development intern at Hindustan Petroleum Corporation Limited during my bachelors. I look forward to this course valuable lessons and becoming a full-stack engineer."
+  );
 }
